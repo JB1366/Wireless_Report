e5bfc532d503f7ce376d7d5ffd3d975a485e91e2
JB1366 <jb8745@proton.me>
2026-03-14 14:39:40 -0500

    v1.1.0

--- install_menu.sh
+++ install_menu.sh
@@ -3,7 +3,7 @@
 #  Wireless Report Menu-TAB Insertion                                        #
 #  Version: 1.1.0                                                            #
 #  Author: JB_1366                                                           #
-#  Revised:                                                                  #
+#  Revised: adjust tab insertion                                             #
 #============================================================================#
 
 source /usr/sbin/helper.sh
@@ -20,7 +20,7 @@ CONF_FILE="$INSTALL_DIR/webui.conf"
 # Does the firmware support addons?
 nvram get rc_support | grep -q am_addons
 if [ $? != 0 ]; then
-    logger "Wireless Report:" "This firmware does not support addons!"
+    logger -t "Wireless Report" "This firmware does not support addons!"
     exit 5
 fi
 
@@ -42,10 +42,9 @@ fi
 am_get_webui_page "$WEB_PAGE"
 
 if [ "$am_webui_page" = "none" ]; then
-    logger "Wireless Report:" "Unable to install Wireless Report"
+    logger -t "Wireless Report" "Unable to install Wireless Report"
     exit 5
 fi
-logger -t "Wireless Report" "Mounting Wireless\Wireless Report as $am_webui_page"
 
 cp "$WEB_PAGE" "/www/user/$am_webui_page"
 
@@ -69,11 +68,6 @@ if [ ! -f "$TEMP_MENU" ]; then
     mount -o bind "$TEMP_MENU" "$SYSTEM_MENU"
 fi
 
-# --- ANTI-DUPLICATE CLEANUP ---
-# Remove any existing lines containing the label or the mount URL to prevent double tabs
-sed -i "/tabName: \"$TAB_LABEL\"/d" "$TEMP_MENU"
-sed -i "/url: \"$am_webui_page\"/d" "$TEMP_MENU"
-
 # Addons Menu-Tab Insertion
 # Insert Wireless Report before HELP menu
 # sed -i "/url: \"javascript:var helpwindow=window.open('\/ext\/shared-jy\/redirect.htm'/i {url: \"$am_webui_page\", tabName: \"$TAB_LABEL\"}," "$TEMP_MENU"
@@ -84,6 +78,7 @@ sed -i "/url: \"$am_webui_page\"/d" "$TEMP_MENU"
 sed -i "/index: \"menu_Wireless\"/,/{url: \"NULL\", tabName: \"__INHERIT__\"}/ {/{url: \"NULL\", tabName: \"__INHERIT__\"}/i \\
 {url: \"$am_webui_page\", tabName: \"$TAB_LABEL\"},
 }" "$TEMP_MENU"
+logger -t "Wireless Report" "Mounting Wireless\Wireless Report as $am_webui_page"
 
 # Remount modified menu
 umount "$SYSTEM_MENU" && mount -o bind "$TEMP_MENU" "$SYSTEM_MENU"
