b3a106b1b95a03c67c9731f80eb89eb8573d409a
JB1366 <jb8745@proton.me>
2026-07-06 15:35:40 -0500

    Wireless Report

--- wirelessreport.sh
+++ wirelessreport.sh
@@ -26,7 +26,7 @@
 #        shellcheck shell=sh disable=SC2086,SC2155,SC3043         #
 #=================================================================#
 
-SCRIPT_VERSION="1.9.8"
+SCRIPT_VERSION="1.9.9"
 INSTALL_DIR="/jffs/addons/wireless_report"
 REPORT_SCRIPT="$INSTALL_DIR/wirelessreport.sh"
 CONFIG="$INSTALL_DIR/webui.conf"
