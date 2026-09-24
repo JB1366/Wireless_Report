c0b4a0cdf200445dc0cf9f5c7bb95ca205b0e7a8
JB1366 <jb8745@proton.me>
2026-09-24 09:10:37 -0500

    remove new top buttons

--- wirelessreport.sh
+++ wirelessreport.sh
@@ -1766,44 +1766,6 @@ cat <<HTML >> "$WEB_PAGE"
         background: rgba(0, 150, 255, 0.15);
     }
 
-
-
-    .btn-tooltip {
-        position: relative;
-    }
-
-    .btn-tooltip:before {
-        content: attr(data-tooltip);
-        position: absolute;
-        height: 28px;
-        line-height: 28px;
-        padding: 0 15px;
-        background: $RT_TOOLTIP;
-        color: white;
-        font-size: 12px;
-        font-weight: bold;
-        border: 1.5px solid #0096ff;
-        border-radius: 20px;
-        box-shadow: 0 0 10px rgba(0, 150, 255, 0.3);
-        white-space: nowrap;
-        opacity: 0;
-        visibility: hidden;
-        transition: all 0.3s ease;
-        z-index: 100;
-        pointer-events: none;
-        top: 150%;
-        left: 150%;
-        transform: translateX(-75%);
-    }
-
-    .btn-tooltip:hover:before {
-        opacity: 1;
-        visibility: visible;
-        top: 150%;
-    }
-
-
-
     #refresh-option {
         color: #ffffff;
         background: transparent;
@@ -2470,9 +2432,6 @@ cat <<HTML >> "$WEB_PAGE"
                             <button id="btnAll" class="button-tables" onclick="switchTab('all')">All Devices</button>
                             <button class="button-tables" onclick="openPopout()" style="">Side by Side ◫</button>
                             <button id="btnWide" class="button-tables" onclick="toggleWideView()">Wide View ⛶</button>
-                            <a class="button-tables btn-tooltip" href="https://github.com/JB1366/Wireless_Report" target="_blank" rel="noopener noreferrer" data-tooltip="Wireless Report Repository">Github</a>
-                            <a class="button-tables btn-tooltip" href="https://www.snbforums.com/threads/97849/latest" target="_blank" rel="noopener noreferrer" data-tooltip="Wireless Report SNB Forums">SNB</a>
-
                         </div>
                     </div>
                     <div class="grid-container">
