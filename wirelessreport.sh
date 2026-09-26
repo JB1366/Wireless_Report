270b796e84176d257f976121f48e9053780d73e9
JB1366 <jb8745@proton.me>
2026-09-25 21:11:55 -0500

    update set_options()

--- wirelessreport.sh
+++ wirelessreport.sh
@@ -1042,10 +1042,6 @@ set_options() {
                         echo -e "\n$GR[+] Adding INJECT=\"2\" to CONFIG.$NC"
                         INJECT="2"
                     fi
-                    if [ ! -f "$SS_FILE" ]; then echo "#!/bin/sh" > "$SS_FILE"; fi
-                    sed -i "\|$REPORT_SCRIPT|d" "$SS_FILE" 2>/dev/null
-                    echo "$REPORT_SCRIPT inject & # Inject Wireless Report" >> "$SS_FILE"
-                    chmod +x "$SS_FILE"
                     inject_menu
                     pause
                     continue 2
